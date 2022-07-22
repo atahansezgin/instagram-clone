a4fb56940769222fa38a6c133e124510555d45bd
Atahan Sezgin <atahan.sezgin@bulutmd.com>
2022-07-23 00:49:57 +0300

    resources created

---
+++ src/resources/Colors.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+  white:"#fff",
+  black:"#000",
+  blue:"#3797EF",
+  border:"#D0D5D9",
+  backgrounWhite:"#fafafa"
+}
---
+++ src/resources/Images.ts
@@ -0,0 +1,3 @@
+export const Images = {
+  statusbar_logo:require("./images/statusbar_logo.png")
+}
